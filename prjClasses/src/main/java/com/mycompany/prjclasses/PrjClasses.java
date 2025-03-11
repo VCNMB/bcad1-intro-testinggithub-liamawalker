d29470d9ded650dc9e2868eaae93d2f66ad09efe
liamawalker <st10467058@vcconnect.edu.za>
2025-03-11 10:49:00 +0200

    initial commit and added classes

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        
+        newClass nc = new newClass(); // creating an object of newClass
+        
+        nc.helloClass();// have to use variable name to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar);
+    }
+}
